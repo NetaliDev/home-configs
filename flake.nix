10e6d7bdaaf6cb524f6521896cd602ddfdc82c73
Jennifer Graul <me@netali.de>
2024-11-28 22:16:30 +0100

    flake: generate outputs dynamically

--- flake.nix
+++ flake.nix
@@ -1,7 +1,17 @@
 {
-  outputs = { self }: {
-    homeConfigs.screen = import ./configs/screen.nix;
-    homeConfigs.vim = import ./configs/vim.nix;
-    homeConfigs.zsh = import ./configs/zsh.nix;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: let
+    lib = nixpkgs.lib;
+    configs = builtins.readDir ./configs;
+    configFiles = lib.filterAttrs (n: v: (lib.hasSuffix ".nix" n) && v == "regular") configs;
+    configFileList = lib.mapAttrsToList (n: _: lib.removeSuffix ".nix" n) configFiles;
+    homeConfigs = lib.listToAttrs (lib.map (x: {
+      name = x;
+      value = import (./configs + "/${x}.nix");
+    }) configFileList);
+  in {
+    inherit homeConfigs;
   };
 }
--- readme.md
+++ readme.md
@@ -12,6 +12,7 @@ This repo currently includes:
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     netali-home-configs.url = "github:netalidev/home-configs";
+    netali-home-configs.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
