8d1c6ecace7fdef5a86367d79e6ed7034b45847b
Jennifer Graul <jgraul@relaix.net>
2026-01-27 18:19:50 +0100

    vim: remove trailing whitespace on save

--- configs/vim.nix
+++ configs/vim.nix
@@ -82,6 +82,16 @@
       map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
       map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
+      -- Remove trailing whitespace on save
+      vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        pattern = {"*"},
+        callback = function(ev)
+          save_cursor = vim.fn.getpos(".")
+          vim.cmd([[%s/\s\+$//e]])
+          vim.fn.setpos(".", save_cursor)
+        end,
+      })
+
       -- Other:
       -- :BarbarEnable - enables barbar (enabled by default)
       -- :BarbarDisable - very bad command, should never be used
